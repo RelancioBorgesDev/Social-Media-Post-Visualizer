72cf4b8a140400a89660ae696400ae9e77d2076f
RelancioBorgesDev <relancio.dev@gmail.com>
2024-03-07 11:47:24 -0300

    feat: adicionado FacebookVisualizer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import {
-  Bookmark,
+  ArrowBendUpRight,
+  GlobeHemisphereWest,
   Heart,
-  MessageCircle,
-  MoreVertical,
-  Send,
-} from "lucide-react";
+  ThumbsUp,
+  WhatsappLogo,
+} from "phosphor-react";
+import Avatar from "./components/Avatar/Avatar";
+import { Dot, MessageCircle, MoreHorizontal } from "lucide-react";
 import imgPost from "./assets/igPostImg.jpg";
 
 function App() {
@@ -18,54 +20,82 @@ function App() {
       </section>
       <section className="w-1/2 border-2 border-white h-screen flex items-center justify-center ">
         {/* Instagram Visualizer */}
-        <div className=" w-[540px]  flex flex-col justify-between shadow-2xl rounded ">
-          {/* Instagram Header */}
-          <header className=" py-8 px-2 w-full h-[90px] flex justify-between items-center bg-white text-zinc-950 rounded-t-lg">
-            <div className="flex gap-4 items-center">
-              <div className="bg-gradient-to-tr from-yellow-400 to-purple-600 p-1 rounded-full">
-                <a
-                  href="#"
-                  className="block bg-white p-1 rounded-full transform transition hover:-rotate-6"
-                >
-                  <img
-                    className="w-16 h-16 rounded-full"
-                    src="https://placekitten.com/200/203"
-                    alt="cute kitty"
-                  />
-                </a>
-              </div>
+        {/*  <InstagramVisualizer /> */}
 
-              <div className="flex flex-col gap-1">
-                <h1 className="text-xl font-bold">sunflower_77</h1>
-                <h3 className="text-xs">Londres</h3>
+        {/* Facebook Visualizer */}
+        <div className=" w-[600px] ">
+          {/* Facebook Header */}
+          <header className="flex flex-col items-center justify-between px-4 py-2 bg-white text-black rounded-t-lg">
+            <section className="w-full flex items-center justify-between bg-white text-black ">
+              <div className="flex items-center gap-3">
+                <Avatar social_media="FB"/>
+                <div className="flex flex-col">
+                  <h1 className="text-xl font-bold">SunFlower_77</h1>
+                  <div className="flex items-center ">
+                    <h3 className="text-sm text-gray-500">
+                      Post feito no brasil
+                    </h3>
+
+                    <Dot size={24} className="text-gray-500" />
+                    <span className="text-sm text-gray-500">Há 2 dias</span>
+                    <Dot size={24} className="text-gray-500" />
+
+                    <GlobeHemisphereWest
+                      size={20}
+                      weight="duotone"
+                      className="text-gray-800"
+                    />
+                  </div>
+                </div>
               </div>
-            </div>
-            <div>
-              <MoreVertical className="text-zinc-950" />
-            </div>
+              <MoreHorizontal className="text-gray-500" />
+            </section>
+            <section>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Similique enim voluptatibus sequi delectus eius in adipisci
+                possimus doloribus ab, amet, ducimus totam. Natus assumenda
+                repellendus, mollitia quas eligendi rem error!
+              </p>
+            </section>
           </header>
-          {/* Instagram Image Wrapper */}
-          <div className="w-full ">
-            <img src={imgPost} alt="" className="w-full h-[500px]" />
+          {/* Facebook Image Wrapper */}
+          <div>
+            <img src={imgPost} alt="Image" className="w-full h-[500px]" />
           </div>
-          {/* Instagram Footer */}
-          <footer className=" bg-white text-zinc-950 px-4 py-4 flex flex-col gap-3 rounded-b-lg">
-            <div className="flex justify-between ">
-              <div className="flex gap-4">
-                <Heart />
-                <MessageCircle />
-                <Send />
+          {/* Facebook Footer */}
+          <footer className="bg-white w-full text-black rounded-b-lg">
+            <div className="flex justify-between items-center px-2 py-4">
+              <div className="flex  items-center  ">
+                <span className="rounded-full bg-blue-600 w-6 h-6 flex items-center justify-center">
+                  <ThumbsUp size={14} className="text-white" weight="fill" />
+                </span>
+                <span className="rounded-full bg-red-600 w-6 h-6 flex items-center justify-center">
+                  <Heart size={14} className="text-white" weight="fill" />
+                </span>
+                <span className="ml-2">77</span>
               </div>
-              <Bookmark />
+              <span>7 comentários</span>
             </div>
-            <span>29.997 curtidas</span>
-            <div className="flex gap-2">
-              <h2 className="font-bold">sunflower_77</h2>
-              <p>Mais uma conquista!!! #Feliz #Carro</p>
+            <hr className="w-full border-t-2 border-gray-200" />
+            <div className="flex justify-around items-center py-2">
+              <span className="flex flex-col gap-2 items-center">
+                <ThumbsUp size={24} />
+                Curtir
+              </span>
+              <span className="flex flex-col gap-2 items-center">
+                <MessageCircle size={24} />
+                Comentar
+              </span>
+              <span className="flex flex-col gap-2 items-center">
+                <WhatsappLogo size={24} />
+                Enviar
+              </span>
+              <span className="flex flex-col gap-2 items-center">
+                <ArrowBendUpRight size={32} />
+                Compartilhar
+              </span>
             </div>
-            <span className="text-xs text-gray-500">
-              Ver todos os 240 comentários
-            </span>
           </footer>
         </div>
       </section>
