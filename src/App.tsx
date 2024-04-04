f3f3e81efa7faae80ed9604da97b28bfe294ac07
RelancioBorgesDev <relancio.dev@gmail.com>
2024-04-04 16:02:38 -0300

    feat: add post state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FacebookLogo, InstagramLogo } from "phosphor-react";
+import { FacebookLogo, Image, InstagramLogo } from "phosphor-react";
 import XVisualizer from "./components/XVisualizer/XVisualizer";
 import { FaXTwitter } from "react-icons/fa6";
 import { ReactNode, useState } from "react";
@@ -7,8 +7,15 @@ import InstagramVisualizer from "./components/InstagramVisualizer/InstagramVisua
 
 type SocialMediaType = "FB" | "IG" | "X" | "";
 
+export interface IPostData {
+  accountName: string;
+  postImage: string;
+  postDescription: string;
+}
+
 function App() {
   const [socialMedia, setSocialMedia] = useState<SocialMediaType>("");
+  const [post, setPost] = useState<IPostData>({} as IPostData);
 
   function handleSetSocialMedia(selectedSocialMedia: SocialMediaType) {
     setSocialMedia(selectedSocialMedia);
@@ -17,25 +24,98 @@ function App() {
   function renderSocialMediaVisualizer(): ReactNode {
     switch (socialMedia) {
       case "FB":
-        return <FacebookVisualizer />;
+        return <FacebookVisualizer post={post} />;
       case "IG":
-        return <InstagramVisualizer />;
+        return <InstagramVisualizer post={post} />;
       case "X":
-        return <XVisualizer />;
+        return <XVisualizer post={post} />;
       default:
         return null;
     }
   }
 
+  function handleAccountNameChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setPost((prevPost) => ({
+      ...prevPost,
+      accountName: event.target.value,
+    }));
+  }
+
+  function handlePostImageChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const file = event.target.files![0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function (event) {
+        if (event.target && event.target.result) {
+          setPost((prevPost) => ({
+            ...prevPost,
+            postImage: event.target!.result!.toString(),
+          }));
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  function handlePostDescriptionChange(
+    event: React.ChangeEvent<HTMLTextAreaElement>
+  ) {
+    setPost((prevPost) => ({
+      ...prevPost,
+      postDescription: event.target.value,
+    }));
+  }
+
   return (
-    <div className="bg-zinc-950 h-screen text-white flex items-center justify-between">
-      <section className="w-1/2 border-2 border-white h-screen flex flex-col items-center justify-center gap-16">
-        <h1 className="text-4xl">Escolha os dados do seu post</h1>
-        <input type="file" />
-        <textarea></textarea>
+    <div className="bg-zinc-700 h-screen text-white flex items-center justify-between ">
+      <section className="w-1/2  h-screen flex flex-col items-center justify-center gap-16">
+        <h1 className="text-4xl font-bold uppercase">
+          Escolha os dados do seu post
+        </h1>
+        <form action="" className="flex flex-col gap-4">
+          <div className="flex justify-between items-center gap-4">
+            <input
+              type="text"
+              className="rounded-sm p-2 outline-none text-black"
+              placeholder="Nome da sua conta"
+              value={post.accountName}
+              onChange={handleAccountNameChange}
+            />
+            <label htmlFor="fileInput">
+              <Image
+                size={64}
+                weight="fill"
+                className="hover:bg-white hover:text-black rounded-full p-4 border-2 border-white"
+              />
+            </label>
+          </div>
+
+          <div className="flex items-center gap-4">
+            <input
+              id="fileInput"
+              type="file"
+              className="hidden"
+              onChange={handlePostImageChange}
+            />
+          </div>
+          <textarea
+            className="w-[600px] h-32 px-4 py-2 border border-gray-300 text-black rounded-md resize-none focus:outline-none focus:border-blue-500"
+            placeholder="Escreva a descrição do seu post"
+            value={post.postDescription}
+            onChange={handlePostDescriptionChange}
+          ></textarea>
+        </form>
       </section>
-      <section className="w-1/2 h-screen flex items-center justify-center flex-col gap-4">
-        <div className="w-full flex items-center justify-center gap-16">
+      <section className="w-1/2 h-screen flex flex-col-reverse items-center justify-center  gap-4 p-16 ">
+        {socialMedia == "" ? (
+          <h1 className="text-4xl font-bold mb-8 text-center">
+            Selecione uma rede social para visualizar seu post
+          </h1>
+        ) : (
+          renderSocialMediaVisualizer()
+        )}
+
+        <div className="w-full flex items-center justify-center gap-16 ">
           <button
             className="rounded-full border-2 border-blue-500 bg-blue-500 flex shadow-2xl p-4 hover:brightness-75 cursor-pointer"
             onClick={() => handleSetSocialMedia("FB")}
@@ -60,8 +140,6 @@ function App() {
             <FaXTwitter size={64} className="text-tranparent" />
           </button>
         </div>
-
-        {renderSocialMediaVisualizer()}
       </section>
     </div>
   );
